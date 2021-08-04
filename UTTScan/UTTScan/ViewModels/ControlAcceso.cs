34201747b56ad31c0b754bc9439c7a5d27ddc919
Marco Jaziel Sanchez Bravo <innova28310@innovaccion.mx>
2021-08-03 23:54:35 -0500

    Registro Entradas y Salidas Api

---
+++ UTTScan/UTTScan/ViewModels/ControlAcceso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTTScan.ViewModels
+{
+    public class ControlAcceso
+    {
+        public DateTime Fecha { get; set; }
+        public int Dia { get; set; }
+        public int Mes { get; set; }
+        public int Anio { get; set; }
+        public int Hora { get; set; }
+        public int Mintos { get; set; }
+        public int FkAlumno { get; set; }
+
+    }
+}
--- UTTScan/UTTScan/Views/VistaQR.xaml.cs
+++ UTTScan/UTTScan/Views/VistaQR.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using UTTScan.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ZXing.Net.Mobile.Forms;
@@ -22,12 +25,36 @@ namespace UTTScan.Views
         {
             try
             {
-                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
-                scanner.TopText = "UTTSCAN";
-                var result = await scanner.Scan();
-                if (result != null)
+                //var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                //scanner.TopText = "UTTSCAN";
+                // var result = await scanner.Scan();
+                if (true)
                 {
-                    txtResultado.Text = result.Text;
+                    Uri requestUri = new Uri("http://utttproyect-001-site1.itempurl.com/api/control-acceso");
+                    var fecha = DateTime.Now;
+                    var client = new HttpClient();
+                    ControlAcceso ctracceso = new ControlAcceso {
+
+                        Fecha = fecha,
+                        Dia = fecha.Day,
+                        Mes = fecha.Month,
+                        Anio = fecha.Year,
+                        Mintos = fecha.Minute,
+                        Hora = fecha.Hour,
+                        FkAlumno = int.Parse("18300181")
+                        
+                    };
+                    var json = JsonConvert.SerializeObject(ctracceso);
+                    var contentjson = new StringContent(json, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync(requestUri, contentjson);
+                    if(response.StatusCode == HttpStatusCode.OK ){
+                        await DisplayAlert("ok", "Acceso Concedido", "ok");
+                    }
+                    else
+                    {
+                        await DisplayAlert("ok No", "No tienes acceso", "ok");
+                    }
+                    // txtResultado.Text = result.Text;
                 }
             }
             catch (Exception ex)
